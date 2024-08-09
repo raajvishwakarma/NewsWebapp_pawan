f4eef808f12ef84901b7dd4017dd02d59ab295d6
Sourabh garewal <57398905+28saurbh@users.noreply.github.com>
2024-08-10 01:17:29 +0530

    add font and remove unused css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  );
+    return (
+        <div className="bg-black" >
+            <div>
+                <div><h2 className='text-white'>NEWSLETTER</h2></div>
+                <div> dfsdf  </div>
+            </div>
+           
+        </div>
+    );
 }
 
 export default App;
